7fb0767e8ea5cf88a381c00070fd527da347b4f0
stiff <15700004138@163.com>
2018-03-21 16:50:36 +0800

    代码格式化

--- Applications/Chat/Events.php
+++ Applications/Chat/Events.php
@@ -23,6 +23,7 @@
  * 聊天主逻辑
  * 主要是处理 onMessage onClose
  */
+
 use \GatewayWorker\Lib\Gateway;
 
 class Events
@@ -33,31 +34,28 @@ class Events
      * @param int $client_id
      * @param mixed $message
      */
-    public static function onMessage($client_id,$message)
+    public static function onMessage($client_id, $message)
     {
 
         // debug
 //        echo "client:{$_SERVER['REMOTE_ADDR']}:{$_SERVER['REMOTE_PORT']} gateway:{$_SERVER['GATEWAY_ADDR']}:{$_SERVER['GATEWAY_PORT']}  client_id:$client_id session:".json_encode($_SESSION)." onMessage:".$message."\n";
 
         // 客户端传递的是加密json数据
-        $message_data = self::crypto($message,2);
+        $message_data = self::crypto($message, 2);
 
-        if(!$message_data)
-        {
-            return ;
+        if (!$message_data) {
+            return;
         }
 
         // 根据类型执行不同的业务
-        switch($message_data['type'])
-        {
+        switch ($message_data['type']) {
             // 客户端回应服务端的心跳
             case 'pong':
-                return ;
+                return;
             // 客户端登录 message格式: {type:login, name:xx, room_id:1} ，添加到客户端，广播给所有客户端xx进入聊天室
             case 'login':
                 // 判断是否有房间号
-                if(!isset($message_data['room_id']))
-                {
+                if (!isset($message_data['room_id'])) {
                     throw new \Exception("\$message_data['room_id'] not set. client_ip:{$_SERVER['REMOTE_ADDR']} \$message:$message");
                 }
                 if (!self::checkData($message_data)) {
@@ -82,21 +80,20 @@ class Events
 
 
                 // 转播给当前房间的所有客户端，xx进入聊天室 message {type:login, client_id:xx, name:xx}
-                $new_message = array('type'=>$message_data['type'], 'client_id'=>$client_id, 'client_name'=>htmlspecialchars($client_name), 'time'=>date('Y-m-d H:i:s'));
-                Gateway::sendToGroup($room_id, self::crypto(json_encode($new_message),1));
+                $new_message = array('type' => $message_data['type'], 'client_id' => $client_id, 'client_name' => htmlspecialchars($client_name), 'time' => date('Y-m-d H:i:s'));
+                Gateway::sendToGroup($room_id, self::crypto(json_encode($new_message), 1));
                 Gateway::joinGroup($client_id, $room_id);
 
                 // 给当前用户发送用户列表
                 $new_message['client_count'] = $clients_list;
 
-                Gateway::sendToCurrentClient(self::crypto(json_encode($new_message),1));
+                Gateway::sendToCurrentClient(self::crypto(json_encode($new_message), 1));
                 return;
 
             // 客户端发言 message: {type:say, to_client_id:xx, content:xx}
             case 'say':
                 // 非法请求
-                if(!isset($_SESSION['room_id']))
-                {
+                if (!isset($_SESSION['room_id'])) {
                     throw new \Exception("\$_SESSION['room_id'] not set. client_ip:{$_SERVER['REMOTE_ADDR']}");
                 }
 
@@ -131,7 +128,7 @@ class Events
                         'content' => nl2br(htmlspecialchars($message_data['content'])),
                         'time' => date('Y-m-d H:i:s'),
                     );
-                    return Gateway::sendToGroup($room_id, self::crypto(json_encode($new_message),1));
+                    return Gateway::sendToGroup($room_id, self::crypto(json_encode($new_message), 1));
                 } else {
 
                     return Gateway::closeCurrentClient();
@@ -149,11 +146,10 @@ class Events
         echo "client:{$_SERVER['REMOTE_ADDR']}:{$_SERVER['REMOTE_PORT']} gateway:{$_SERVER['GATEWAY_ADDR']}:{$_SERVER['GATEWAY_PORT']}  client_id:$client_id onClose:''\n";
 
         // 从房间的客户端列表中删除
-        if(isset($_SESSION['room_id']))
-        {
+        if (isset($_SESSION['room_id'])) {
             $room_id = $_SESSION['room_id'];
-            $new_message = array('type'=>'logout', 'from_client_id'=>$client_id, 'from_client_name'=>$_SESSION['client_name'], 'time'=>date('Y-m-d H:i:s'));
-            Gateway::sendToGroup($room_id, self::crypto(json_encode($new_message),1));
+            $new_message = array('type' => 'logout', 'from_client_id' => $client_id, 'from_client_name' => $_SESSION['client_name'], 'time' => date('Y-m-d H:i:s'));
+            Gateway::sendToGroup($room_id, self::crypto(json_encode($new_message), 1));
         }
     }
 
@@ -165,7 +161,7 @@ class Events
         $token = 'abcdefghijklmnopqrstuvwxyz*.123456';
 
         // 验证发送时间 Socket连接到进入房间不能超过五秒
-        if (!$data['_timestamp'] || (abs($_SESSION['start_time'] - $data['_timestamp']) >5 && $data['type'] !== 'say')) {
+        if (!$data['_timestamp'] || (abs($_SESSION['start_time'] - $data['_timestamp']) > 5 && $data['type'] !== 'say')) {
             echo '时间错了';
 
             return false;
@@ -173,7 +169,7 @@ class Events
             echo 'token错了';
 
             return false;
-        } else if($data['type'] !== 'say' && !self::checkMd5($data) ){
+        } else if ($data['type'] !== 'say' && !self::checkMd5($data)) {
             echo '加密错了';
 
             return false;
@@ -192,13 +188,11 @@ class Events
     {
         $md5 = $data['_checksum'];
         unset($data['_checksum']);
-        $data['_secret'] = $data['_token']."zt".$data['_timestamp'];
+        $data['_secret'] = $data['_token'] . "zt" . $data['_timestamp'];
         ksort($data);
         $stringfy = '';
-        foreach ($data as $k=>$v)
-        {
-            if($stringfy)
-            {
+        foreach ($data as $k => $v) {
+            if ($stringfy) {
                 $stringfy .= '&';
             }
             $stringfy .= "{$k}={$v}";
@@ -216,16 +210,16 @@ class Events
     /**
      * openssl 加密解密
      */
-    private static function crypto($data,$type)
+    private static function crypto($data, $type)
     {
         $key = '*zt$zhengwu%win.';
         $iv = "hahahaha!@#$%^&*";
         if ($type == 1) { // 加密
-            $data = openssl_encrypt($data, 'aes-128-cbc', $key, false , $iv);
+            $data = openssl_encrypt($data, 'aes-128-cbc', $key, false, $iv);
 //            echo '加密输出'.$data;
 
         } else { // 解密
-            $data = openssl_decrypt($data, 'aes-128-cbc', $key, OPENSSL_ZERO_PADDING , $iv);
+            $data = openssl_decrypt($data, 'aes-128-cbc', $key, OPENSSL_ZERO_PADDING, $iv);
             $data = trim($data);
             $data = json_decode($data, true);
         }
@@ -233,7 +227,7 @@ class Events
         return $data;
     }
 
-    private static function crypto1($data,$type)
+    private static function crypto1($data, $type)
     {
         $privateKeyFilePath = 'rsa_private_key.pem';
         $privateKey = openssl_pkey_get_private(file_get_contents($privateKeyFilePath));
